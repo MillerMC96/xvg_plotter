71ce3e1d8f85165939fb81d3e6de75a39fb6f12c
MillerMC96 <pywang@ucdavis.edu>
2020-09-22 17:21:30 -0700

    initialize

---
+++ xvg_plotter.py
@@ -0,0 +1,7 @@
+###############################################################################
+# matplotlib API for xvg format files
+# Author: Panyue Wang (pywang@ucdavis.edu)
+###############################################################################
+
+import numpy as np
+import matplotlib.pyplot as plt
