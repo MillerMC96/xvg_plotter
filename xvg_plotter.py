db8f06cfb6ebb27805d74d7c56ffaadc10eb82ab
MillerMC96 <pywang@ucdavis.edu>
2020-09-22 17:26:32 -0700

    framework

--- xvg_plotter.py
+++ xvg_plotter.py
@@ -5,3 +5,15 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+
+# read xvg file
+# data entry
+for line in lines:
+    line_entry = line.split()
+    # skip comments
+    first_charactor = line_entry[0]
+    if first_charactor[0] != '#' and first_charactor[0] != '@':
+
+# set plot parameters
+
+# main function
