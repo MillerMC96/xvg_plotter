a83881232be718b93f50e43e6bd365d24146c017
MillerMC96 <pywang@ucdavis.edu>
2020-09-22 17:37:34 -0700

    xvg read finished

--- xvg_plotter.py
+++ xvg_plotter.py
@@ -7,12 +7,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # read xvg file
-# data entry
-for line in lines:
-    line_entry = line.split()
-    # skip comments
-    first_charactor = line_entry[0]
-    if first_charactor[0] != '#' and first_charactor[0] != '@':
+
+def read_xvg(xvg_file):
+    xvg_file_obj = open(xvg_file, 'r')
+    lines = xvg_file_obj.readlines()
+    # x axis
+    x = []
+    # y axis
+    y = []
+    # data entry
+    for line in lines:
+        line_entry = line.split()
+        # skip comments
+        first_charactor = line_entry[0]
+        if first_charactor[0] != '#' and first_charactor[0] != '@':
+            x.append(float(line_entry[0]))
+            y.append(float(line_entry[1]))
+
+    return x, y
+
+
 
 # set plot parameters
 
